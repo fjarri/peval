00c7841f5e60a673c3ea517c003a3aeaba758fd5
Bogdan Opanchuk <bogdan@opanchuk.net>
2016-01-28 21:10:31 +1100

    docs/conf: updated the copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'peval'
-copyright = u'2014, Bogdan Opanchuk'
+copyright = u'2016, Bogdan Opanchuk'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
